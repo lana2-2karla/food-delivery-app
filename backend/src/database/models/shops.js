359d146eeebc75936460d222270660b8667b9d23
lana2-2karla <allanakarla1702@gmail.com>
2022-12-01 15:41:59 -0300

    :sparkles: feat: creates the Shops table model

---
+++ backend/src/database/models/shops.js
@@ -0,0 +1,38 @@
+const userSchema = (sequelize, DataTypes) => {
+  const userTable = sequelize.define('Shop', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    rating: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    deliveryTime: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    deliveryValue: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    urlImage: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, { timestamps: false })
+
+  return userTable
+}
+
+module.exports = userSchema
