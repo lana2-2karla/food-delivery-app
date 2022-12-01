ebceaa547d18aef5e1a62847ce78065df9277185
lana2-2karla <allanakarla1702@gmail.com>
2022-12-01 12:49:09 -0300

    :recycle: refactor: add .env in index

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const routes = require('./routes')
@@ -6,4 +8,4 @@ app.use(express.json())
 
 app.use(routes)
 
-app.listen(3000, () => console.log('ouvindo na porta 3000'))
+app.listen(process.env.APP_PORT, () => console.log(`ouvindo na porta ${process.env.APP_PORT}`))
