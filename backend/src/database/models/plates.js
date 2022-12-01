cf3c5f1b4517a801942122772f8859803bd84e19
lana2-2karla <allanakarla1702@gmail.com>
2022-12-01 16:24:38 -0300

    :sparkles: feat: creates the Shops table models

---
+++ backend/src/database/models/plates.js
@@ -0,0 +1,34 @@
+const userSchema = (sequelize, DataTypes) => {
+  const plateTable = sequelize.define('Plate', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    ingredients: {
+      allowNull: false,
+      type: DataTypes.JSON
+    },
+    value: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    urlImage: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    shopId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    }
+  }, { timestamps: false })
+
+  return plateTable
+}
+
+module.exports = userSchema
