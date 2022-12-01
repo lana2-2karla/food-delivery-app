fa97f5b178ea7d76c2d8ef9dcb5c5d2d20730532
lana2-2karla <allanakarla1702@gmail.com>
2022-12-01 16:25:36 -0300

    :sparkles: feat: creates the Plates table migrations

---
+++ backend/src/database/migrations/20221201184651-create-plates.js
@@ -0,0 +1,49 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Plates', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      ingredients: {
+        allowNull: false,
+        type: Sequelize.JSON
+      },
+      value: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      urlImage: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      shopId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Shops',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Plates')
+  }
+}
