702210b85ccbc838e001ff6a5a84f947644d40fb
lana2-2karla <allanakarla1702@gmail.com>
2022-12-02 12:07:51 -0300

    :recycle: refactor: add the url of the plates image

--- backend/src/database/seeders/20221202125038-demo-plates.js
+++ backend/src/database/seeders/20221202125038-demo-plates.js
@@ -2,46 +2,46 @@ module.exports = {
   up: async (queryInterface, _Sequelize) => {
     await queryInterface.bulkInsert('Plates',
       [{
-        name: 'MABO NASU',
+        name: 'Mabo Nasu',
         ingredients: 'berinjela,carne moída de porco,tenmenjang,toubanjang',
         value: '60.00',
-        urlImage: 'image',
-        shopId: 1
+        urlImage: 'https://sudachirecipes.com/wp-content/uploads/2022/08/mabo-nasu-thumbnail.jpg',
+        shopId: 41
       },
       {
-        name: 'TAKIKOMI GOHAN',
+        name: 'Takikomi Gohan',
         ingredients: 'arroz japonês,salmão,cogumelos shiitake,cenoura',
         value: '47.00',
-        urlImage: 'image',
-        shopId: 1
+        urlImage: 'https://i0.wp.com/misomemoirs.com/wp-content/uploads/2021/04/IMG_9408-main.jpg?w=1200&ssl=1',
+        shopId: 41
       },
       {
-        name: 'KANTAN SHOUGAYAKI',
-        ingredients: 'carne de porco,shoyu,gengibre,tomate',
+        name: 'Tofu Fedorento',
+        ingredients: 'soja,vinho de arroz,sal,vinagre',
         value: '38.00',
-        urlImage: 'image',
-        shopId: 1
+        urlImage: 'https://img.freepik.com/fotos-gratis/um-lanche-chines-tofu-fedorento_1205-9155.jpg',
+        shopId: 41
       },
       {
         name: 'Burrito de Frango',
         ingredients: 'arroz,feijão,frango,molho citronete',
         value: '56.11',
-        urlImage: 'image',
-        shopId: 2
+        urlImage: 'https://dd7gu835n6agp.cloudfront.net/wp-content/uploads/2021/10/burrito-de-frango-com-guacamole-889x500.jpg',
+        shopId: 42
       },
       {
         name: 'Camarão tailandês',
         ingredients: 'curry,gengibre,abobrinha,camarão',
         value: '43.20',
-        urlImage: 'image',
-        shopId: 3
+        urlImage: 'https://www.entrepratosecopos.com.br/storage/receitas/imagem1_6943.jpg',
+        shopId: 43
       },
       {
         name: 'Frango com quiabo',
         ingredients: 'frango,quiabo,cebolinha,salsinha',
         value: '27.80',
-        urlImage: 'image',
-        shopId: 4
+        urlImage: 'http://www.saboresajinomoto.com.br/uploads/images/recipes/frango-com-quiabo-1.jpg',
+        shopId: 44
       }
       ], { timestamps: false })
   },
