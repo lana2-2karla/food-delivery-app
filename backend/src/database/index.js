19aa0d11928f360a88f78685be28cb6827ed942c
lana2-2karla <allanakarla1702@gmail.com>
2022-12-02 20:23:49 -0300

    :truck: chore: move index to models folder

--- backend/src/database/index.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize')
-const configDB = require('../database/config')
-
-require('../database')
-
-const connection = new Sequelize(configDB)
-
-module.exports = connection
