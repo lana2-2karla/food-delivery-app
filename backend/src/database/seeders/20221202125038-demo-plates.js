deb6c8bb66491f16001d8238c6458d36f69d5159
lana2-2karla <allanakarla1702@gmail.com>
2022-12-02 20:28:01 -0300

    :recycle: refactor: shopId columns

--- backend/src/database/seeders/20221202125038-demo-plates.js
+++ backend/src/database/seeders/20221202125038-demo-plates.js
@@ -6,42 +6,42 @@ module.exports = {
         ingredients: 'berinjela,carne moída de porco,tenmenjang,toubanjang',
         value: '60.00',
         urlImage: 'https://sudachirecipes.com/wp-content/uploads/2022/08/mabo-nasu-thumbnail.jpg',
-        shopId: 41
+        shopId: 1
       },
       {
         name: 'Takikomi Gohan',
         ingredients: 'arroz japonês,salmão,cogumelos shiitake,cenoura',
         value: '47.00',
         urlImage: 'https://i0.wp.com/misomemoirs.com/wp-content/uploads/2021/04/IMG_9408-main.jpg?w=1200&ssl=1',
-        shopId: 41
+        shopId: 1
       },
       {
         name: 'Tofu Fedorento',
         ingredients: 'soja,vinho de arroz,sal,vinagre',
         value: '38.00',
         urlImage: 'https://img.freepik.com/fotos-gratis/um-lanche-chines-tofu-fedorento_1205-9155.jpg',
-        shopId: 41
+        shopId: 1
       },
       {
         name: 'Burrito de Frango',
         ingredients: 'arroz,feijão,frango,molho citronete',
         value: '56.11',
         urlImage: 'https://dd7gu835n6agp.cloudfront.net/wp-content/uploads/2021/10/burrito-de-frango-com-guacamole-889x500.jpg',
-        shopId: 42
+        shopId: 2
       },
       {
         name: 'Camarão tailandês',
         ingredients: 'curry,gengibre,abobrinha,camarão',
         value: '43.20',
         urlImage: 'https://www.entrepratosecopos.com.br/storage/receitas/imagem1_6943.jpg',
-        shopId: 43
+        shopId: 3
       },
       {
         name: 'Frango com quiabo',
         ingredients: 'frango,quiabo,cebolinha,salsinha',
         value: '27.80',
         urlImage: 'http://www.saboresajinomoto.com.br/uploads/images/recipes/frango-com-quiabo-1.jpg',
-        shopId: 44
+        shopId: 4
       }
       ], { timestamps: false })
   },
