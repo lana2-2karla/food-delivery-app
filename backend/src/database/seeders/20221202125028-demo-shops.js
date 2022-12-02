e3f5707b107954ed9d5beb0ccdfa9d22f8050897
lana2-2karla <allanakarla1702@gmail.com>
2022-12-02 12:07:22 -0300

    :recycle: refactor: add the url of the shops image

--- backend/src/database/seeders/20221202125028-demo-shops.js
+++ backend/src/database/seeders/20221202125028-demo-shops.js
@@ -7,7 +7,7 @@ module.exports = {
         rating: '3.7',
         deliveryTime: '40-50',
         deliveryValue: '6.00',
-        urlImage: 'image'
+        urlImage: 'https://www.gramadooficial.com.br/wp-content/uploads/2021/11/shutterstock_1937661397-scaled.jpg'
       },
       {
         name: 'Totopos',
@@ -15,7 +15,7 @@ module.exports = {
         rating: '3.1',
         deliveryTime: '5-10',
         deliveryValue: '2.00',
-        urlImage: 'image'
+        urlImage: 'https://img.itdg.com.br/tdg/images/blog/uploads/2022/04/shutterstock_2067116069.jpg'
       },
       {
         name: 'Go Thai!',
@@ -23,7 +23,7 @@ module.exports = {
         rating: '4.5',
         deliveryTime: '40-50',
         deliveryValue: '7.00',
-        urlImage: 'image'
+        urlImage: 'https://img.taste.com.au/-rXeYPJs/taste/2016/11/chicken-pad-thai-94082-1.jpeg'
       },
       {
         name: 'Stilo Mineiro',
@@ -31,7 +31,7 @@ module.exports = {
         rating: '4.2',
         deliveryTime: '20-30',
         deliveryValue: '4.50',
-        urlImage: 'image'
+        urlImage: 'https://img.itdg.com.br/tdg/images/recipes/000/069/468/358656/358656_original.jpg?mode=crop&width=710&height=400'
       }
       ], { timestamps: false })
   },
