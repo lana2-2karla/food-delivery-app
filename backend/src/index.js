bce6ba76834e2d95fd81d71b33fa0e6cccce42ff
lana2-2karla <allanakarla1702@gmail.com>
2022-12-02 13:42:06 -0300

    :sparkles: feat: import middleware in index

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,12 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const routes = require('./routes')
+const errorMiddleware = require('./middlewares/error')
 
 app.use(express.json())
 
 app.use(routes)
+
+app.use(errorMiddleware)
 
 app.listen(process.env.APP_PORT, () => console.log(`ouvindo na porta ${process.env.APP_PORT}`))
