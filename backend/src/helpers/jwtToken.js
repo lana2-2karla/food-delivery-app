14e5296e52f496501038c01f3757d51597459bd2
lana2-2karla <allanakarla1702@gmail.com>
2022-12-02 14:27:27 -0300

    :sparkles: feat: create function that decodes token

--- backend/src/helpers/jwtToken.js
+++ backend/src/helpers/jwtToken.js
@@ -1,4 +1,4 @@
-const { sign } = require('jsonwebtoken')
+const { sign, verify } = require('jsonwebtoken')
 require('dotenv').config()
 
 const SECRET = process.env.JWT_SECRET || ''
@@ -12,6 +12,14 @@ const generateToken = async (payload) => {
   return sign(payload, SECRET, jwtConfig)
 }
 
+const decodedToken = (token) => {
+  const payload = verify(token, SECRET)
+  const status = { status: 401, message: 'invalid Token' }
+  if (!payload) throw status
+  return payload
+}
+
 module.exports = {
-  generateToken
+  generateToken,
+  decodedToken
 }
