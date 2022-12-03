5f048277f851b752d6d033dfb087f76826e93fce
lana2-2karla <allanakarla1702@gmail.com>
2022-12-03 17:03:51 -0300

    :sparkles: feat: capture shops and dishes - controller

---
+++ backend/src/controllers/plates.controller.js
@@ -0,0 +1,14 @@
+const { getAllPlatesAndShopsService } = require('../services/plates.service')
+
+const getAllPlatesAndShopsController = async (req, res) => {
+  try {
+    const infoComplete = await getAllPlatesAndShopsService(req.body)
+    return res.status(200).json(infoComplete)
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' })
+  }
+}
+
+module.exports = {
+  getAllPlatesAndShopsController
+}
