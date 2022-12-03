ed45026e0dd509d1a03564f7aa69c67700b5671e
lana2-2karla <allanakarla1702@gmail.com>
2022-12-03 13:08:24 -0300

    :bug: fix: no error handling - user controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,8 +1,14 @@
 const { createUserService } = require('../services/user.service')
 
 const createUserController = async (req, res) => {
-  const message = await createUserService(req.body)
-  return res.status(200).json(message)
+  try {
+    const message = await createUserService(req.body)
+    return message.status
+      ? res.status(message.status).json(message.message)
+      : res.status(201).json(message)
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' })
+  }
 }
 
 module.exports = {
