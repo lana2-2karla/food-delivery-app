e14dfa19a068f058474ceb85ebc0d21c8f36716e
lana2-2karla <allanakarla1702@gmail.com>
2022-12-03 17:07:25 -0300

    :sparkles: feat: import cors in index

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const routes = require('./routes')
 const errorMiddleware = require('./middlewares/error')
+
+app.use(cors())
 
 app.use(express.json())
 
