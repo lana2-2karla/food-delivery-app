51484cdeb62c7a2c56f1fda113bab06014c297a3
lana2-2karla <allanakarla1702@gmail.com>
2022-12-03 13:07:06 -0300

    :recycle: refactor: passwords already hashed

--- backend/src/database/seeders/20221202125017-demo-users.js
+++ backend/src/database/seeders/20221202125017-demo-users.js
@@ -4,17 +4,20 @@ module.exports = {
       [{
         firstName: 'Fred',
         email: 'fred@graodireto.com.br',
-        password: '123Fred@'
+        password: '$2a$12$u/J3Gb/.5Lzi/oUwTzOG3eEgbtej1C4G84fY2./hqa3AbsV/3/Pda'
+        // 123Fred@
       },
       {
         firstName: 'Lana',
         email: 'lana@graodireto.com.br',
-        password: '123Lana@'
+        password: '$2a$12$AYyPh6MCF5/3jxk0UzJgNOIrHSScybbAOVi5yeR8alVlkF87Wv5.q'
+        // 123Lana@
       },
       {
         firstName: 'Nessa',
         email: 'nessa@graodireto.com.br',
-        password: '123Nessa@'
+        password: '$2a$12$y8YIFbvLuWG52E4BCkqbH.t6WMGzY3RCYLFv1PxLEKKRJOO3LreEm'
+        // 123Nessa@
       }
       ], { timestamps: false })
   },
