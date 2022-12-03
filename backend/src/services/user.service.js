2ecbca2181659b5985962d666e62d8f45939b446
lana2-2karla <allanakarla1702@gmail.com>
2022-12-03 13:08:52 -0300

    :bug: fix: no error handling - user service

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -1,14 +1,16 @@
 const { Users } = require('../database/models')
-const { bcrypt } = require('bcrypt')
+const bcrypt = require('bcrypt')
 
 const createUserService = async (userData) => {
+  console.log('cheguei no service')
   const userExists = await Users.findOne({
     where: {
       email: userData.email
     }
   })
+
   const status = { status: 400, message: 'email already exists' }
-  if (userExists) throw status
+  if (userExists) return status
 
   // segundo parâmetro é o nível de processamento do pc p/ gerar a hash
   const hashPassword = await bcrypt.hash(userData.password, 10)
