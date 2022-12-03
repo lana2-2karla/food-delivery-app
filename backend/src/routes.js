b5fcb900c8e6fc46629c2633119432426ea9aafd
lana2-2karla <allanakarla1702@gmail.com>
2022-12-03 17:05:05 -0300

    :sparkles: feat: router /plates - method GET

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express')
 const { loginController } = require('./controllers/login.controller')
+const { getAllPlatesAndShopsController } = require('./controllers/plates.controller')
 const { createUserController } = require('./controllers/user.controller')
 const authUser = require('./middlewares/registerValidate')
 
@@ -8,5 +9,7 @@ const router = Router()
 router.post('/login', loginController)
 
 router.post('/user', authUser, createUserController)
+
+router.get('/plates', getAllPlatesAndShopsController)
 
 module.exports = router
