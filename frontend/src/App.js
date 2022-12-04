9defe0be00db950ef6303699c194454bd45dc1d6
lana2-2karla <allanakarla1702@gmail.com>
2022-12-04 15:23:43 -0300

    :ok_hand: style: lint - app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import Login from "./pages/Login";
+import React from 'react'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import Login from './pages/Login'
 
-
-function App() {
+function App () {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={ <Login /> }/>
     </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
