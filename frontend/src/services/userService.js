19e9e6398dd92322ec7a073155e6a37d918efcb0
lana2-2karla <allanakarla1702@gmail.com>
2022-12-04 20:20:08 -0300

    :spakles: feat: create class UserServices and method login

---
+++ frontend/src/services/userService.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-useless-return */
+import axios from 'axios'
+
+export default class UserServices {
+  constructor () {
+    this.baseUrl = 'http://localhost:3001'
+  }
+
+  async login (dados) {
+    await axios.post(`${this.baseUrl}/login`, {
+      email: dados.email,
+      password: dados.password
+    })
+      .then(function (response) {
+        console.log(response)
+        localStorage.setItem('email', dados.email)
+        localStorage.setItem('password', dados.password)
+        localStorage.setItem('token', response)
+      })
+      .catch(function (error) {
+        console.error(error)
+      })
+  }
+}
