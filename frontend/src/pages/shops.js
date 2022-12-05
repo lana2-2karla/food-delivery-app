f8b1983cdd618483dc5289009c3fc5223aef9360
lana2-2karla <allanakarla1702@gmail.com>
2022-12-05 20:26:42 -0300

    :recycle: refactor: failed attempt to center input

--- frontend/src/pages/shops.js
+++ frontend/src/pages/shops.js
@@ -6,7 +6,7 @@ function Shops () {
   return (
     <div>
       <Header />
-      <div className="flex justify-center">
+      <div className="md:flex items-center">
       <input
         className="p-2 mt-8 rounded-xl w-3/5 border-color: rgb(0 0 0) border-2"
         type="text" name="search"
