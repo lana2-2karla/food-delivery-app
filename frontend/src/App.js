9f22c7e92c54278e8fc8d35b2d5c9ee3a9629253
lana2-2karla <allanakarla1702@gmail.com>
2022-12-05 13:12:22 -0300

    :sparkles: feat: create route /shops

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Login from './pages/Login'
+import Shops from './pages/shops'
 
 function App () {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={ <Login /> }/>
+      <Route path="/shops" element={ <Shops /> }/>
     </Routes>
     </BrowserRouter>
   )
