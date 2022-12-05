a1f8691d6833dfa38998b00f5ccd58036d3d94f6
lana2-2karla <allanakarla1702@gmail.com>
2022-12-05 17:33:29 -0300

    :sparkles: feat: create input shops

--- frontend/src/pages/shops.js
+++ frontend/src/pages/shops.js
@@ -3,7 +3,16 @@ import Header from '../components/header'
 
 function Shops () {
   return (
-    <Header />
+    <div>
+      <Header />
+      <div className="flex justify-center">
+      <input
+        className="p-2 mt-8 rounded-xl w-3/5 border-color: rgb(0 0 0) border-2"
+        type="text" name="search"
+        placeholder="search"
+      />
+      </div>
+    </div>
   )
 }
 export default Shops
