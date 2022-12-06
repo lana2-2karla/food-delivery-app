04ca345fa0971efa3f3f74fa5cbb2012e71410a5
lana2-2karla <allanakarla1702@gmail.com>
2022-12-06 16:00:19 -0300

    :sparkles: feat: import provider in index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import FoodProvider from './contexts/foodContext'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
+    <FoodProvider>
     <App />
+    </FoodProvider>
   </React.StrictMode>
 )
