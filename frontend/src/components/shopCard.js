f7b5620e935f4fc7826f03a6b2bb1f807e237eb9
lana2-2karla <allanakarla1702@gmail.com>
2022-12-06 19:49:24 -0300

    :sparkles: feat: full filter

--- frontend/src/components/shopCard.js
+++ frontend/src/components/shopCard.js
@@ -11,17 +11,23 @@ function ShopCard (props) {
   }, Object.create(null))
 
   return (
-    <div className="min-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-10">
+    <div className="min-w-md mx-auto bg-white rounded-xl overflow-hidden p-10 flex flex-col md:flex-row md:max-w-5xl md:flex-wrap">
       {uniquesFood
-        .filter((infoFood) => infoFood.Shop.name.includes(searchValue)).map((data, index) => (
-          <div key={index} className="md:flex rounded-xl">
+        .filter((infoFood) => infoFood.Shop.name.toLowerCase().includes(searchValue.toLowerCase()) ||
+        infoFood.name.toLowerCase().includes(searchValue.toLowerCase()) ||
+        infoFood.Shop.description.toLowerCase().includes(searchValue.toLowerCase()))
+        .map((data, index) => (
+          <div key={index} className="md:flex rounded-xl md:m-5 shadow-xl">
               <div className="md:shrink-0">
                   <img className="h-28 w-full object-cover md:h-full md:w-48 " src={data.Shop.urlImage} alt="foodImage"/>
               </div>
               <div className="p-8">
                 <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{data.Shop.name}</div>
                   <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{data.Shop.description}</a>
+                  <div className="flex items-stretch md:items-center">
+                  <img className="h-2 position: relative top-3.5 md:relative md:top-1.5" src='https://cdn-icons-png.flaticon.com/512/148/148841.png' alt="startIcons"/>
                   <p className="mt-2 text-slate-500">{data.Shop.rating} - {data.Shop.deliveryTime}m - ${data.Shop.deliveryValue}</p>
+                  </div>
               </div>
           </div>
         ))
