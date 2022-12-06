154ef1c1a1458b7a4aad79f9e450cd1d7045fe19
lana2-2karla <allanakarla1702@gmail.com>
2022-12-06 16:57:01 -0300

    :construction: filter shops

--- frontend/src/components/shopCard.js
+++ frontend/src/components/shopCard.js
@@ -3,22 +3,24 @@ import React, { useContext } from 'react'
 
 /* eslint-disable react/prop-types */
 function ShopCard (props) {
+  const { searchValue } = props
   const { food } = useContext(FoodContext)
 
   return (
     <div className="min-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-10">
-      {food.map((data, index) => (
-        <div key={index} className="md:flex rounded-xl">
-            <div className="md:shrink-0">
-                <img className="h-28 w-full object-cover md:h-full md:w-48 " src="https://www.wbcsd.org/var/site/storage/images/media/images/fresh_pa/80809-2-eng-GB/FRESH_PA_i1140.jpg" alt="Modern building architecture"/>
-            </div>
-            <div className="p-8">
-               <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Stilo Mineiro</div>
-                <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Comida Brasileira</a>
-                <p className="mt-2 text-slate-500">4.3 - 30-20m - $3,00</p>
-            </div>
-       </div>
-      ))
+      {food
+        .filter((infoFood) => infoFood.Shop.name.includes(searchValue)).map((data, index) => (
+          <div key={index} className="md:flex rounded-xl">
+              <div className="md:shrink-0">
+                  <img className="h-28 w-full object-cover md:h-full md:w-48 " src={data.Shop.urlImage} alt="foodImage"/>
+              </div>
+              <div className="p-8">
+                <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{data.Shop.name}</div>
+                  <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{data.Shop.description}</a>
+                  <p className="mt-2 text-slate-500">{data.Shop.rating} - {data.Shop.deliveryTime}m - ${data.Shop.deliveryValue}</p>
+              </div>
+          </div>
+        ))
       }
     </div>
   )
