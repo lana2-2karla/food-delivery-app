cebac04f4fc1d6942dd02a342ab5b7f355d0d080
lana2-2karla <allanakarla1702@gmail.com>
2022-12-07 17:12:38 -0300

    :sparkles: feat: implement and style plateCardDetails

--- frontend/src/components/platesCard.js
+++ frontend/src/components/platesCard.js
@@ -3,9 +3,25 @@ import React from 'react'
 
 function PlateCardDetail (props) {
   const { plates } = props
-  console.log(plates)
+
   return (
-        <div> Card Pratos</div>
+      <div className="flex shadow-lg md:shadow-lg md:pl-72 md:pt-10 pt-10">
+                <div className="absolute md:pl-10 pl-10">
+                    <h3 className=" text-xl">Pratos Executivos:</h3>
+                </div>
+         <div className="p-10 inline-flex md:w-1/4 md:flex md:pt-20">
+              <div className="w-1/2 flex justify-center flex-wrap">
+                <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{plates.name}</div>
+                  <img className="" src={plates.urlImage} alt="foodImage"/>
+              </div>
+                  <div className="w-1/2 p-5 pl-10">
+                {plates.ingredients.map((plate, index) => (
+                    <h2 key={index} className="">{plate},</h2>
+                ))}
+                    <p className="mt-3 text-slate-500">R${plates.value}</p>
+                  </div>
+          </div>
+    </div>
   )
 }
 export default PlateCardDetail
