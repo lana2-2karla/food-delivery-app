92b115651868b09a895b15fb382dd66d7fb2b805
lana2-2karla <allanakarla1702@gmail.com>
2022-12-07 10:41:13 -0300

    :sparkles: feat: capture information by useLocation

--- frontend/src/pages/ShopsDetails.js
+++ frontend/src/pages/ShopsDetails.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import Header from '../components/header'
+import { useLocation } from 'react-router-dom'
 
 function ShopDetails () {
+  const location = useLocation()
+  const data = location.state.data
+  console.log(data, 'dataaaaaaaa')
+
   return (
     <div>
       <Header />
