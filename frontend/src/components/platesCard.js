e4aa98021e1ea9a3fca5dc9e65c7b05373baab82
lana2-2karla <allanakarla1702@gmail.com>
2022-12-07 14:46:55 -0300

    :sparkles: feat: receive dish information

--- frontend/src/components/platesCard.js
+++ frontend/src/components/platesCard.js
@@ -1,6 +1,9 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
 
 function PlateCardDetail (props) {
+  const { plates } = props
+  console.log(plates)
   return (
         <div> Card Pratos</div>
   )
