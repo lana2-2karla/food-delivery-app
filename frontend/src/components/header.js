4e2560ced9fd2e3a51425225616c3b82872791f2
lana2-2karla <allanakarla1702@gmail.com>
2022-12-07 20:25:06 -0300

    :sparkles: feat: add link to shop

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -16,7 +16,7 @@ function Header () {
 
     <ul className="md:flex md:items-center z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
       <li className="mx-4 my-6 md:my-0">
-        <a href="#" className="text-lg hover:text-red-700 duration-500">HOME</a>
+        <a href="/shops" className="text-lg hover:text-red-700 duration-500">HOME</a>
       </li>
       <li className="mx-4 my-6 md:my-0">
         <a href="#" className="text-lg  hover:text-red-700 duration-500">ABOUT</a>
