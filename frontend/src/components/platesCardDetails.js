95eddff7dac804602b28783a4cb1d2e7894a4a81
lana2-2karla <allanakarla1702@gmail.com>
2022-12-07 20:06:58 -0300

    :recycle: refactor: add shadow to card

--- frontend/src/components/platesCardDetails.js
+++ frontend/src/components/platesCardDetails.js
@@ -5,11 +5,11 @@ function PlateCardDetails (props) {
   const { plates } = props
 
   return (
-      <div className="flex md:flex shadow-lg md:shadow-xl md:pl-72 md:pt-10 pt-10">
+      <div className="flex md:flex md:pl-72 md:pt-10 pt-10">
                 <div className="absolute md:pl-10 pl-10">
                     <h3 className=" text-xl">Pratos Executivos:</h3>
                 </div>
-         <div className="p-10 inline-flex md:w-1/4 md:flex md:pt-20">
+         <div className="p-10 inline-flex md:w-1/4 md:flex md:pt-20 shadow-lg">
               <div className="w-1/2 flex justify-center flex-wrap">
                 <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{plates.name}</div>
                   <img className="" src={plates.urlImage} alt="foodImage"/>
