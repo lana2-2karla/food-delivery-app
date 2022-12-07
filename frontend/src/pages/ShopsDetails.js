1ad2c2b198b245e819964648a7bfbb6fe0c77480
lana2-2karla <allanakarla1702@gmail.com>
2022-12-07 10:04:25 -0300

    :sparkles: feat: create page ShopsDetails

---
+++ frontend/src/pages/ShopsDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../components/header'
+
+function ShopDetails () {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+export default ShopDetails
