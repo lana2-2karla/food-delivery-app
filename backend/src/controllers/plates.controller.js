601a09b0b7cdf1869524215875eafb4007513ae5
lana2-2karla <allanakarla1702@gmail.com>
2022-12-13 16:26:17 -0300

    :bug: fix: withdrawal of request

--- backend/src/controllers/plates.controller.js
+++ backend/src/controllers/plates.controller.js
@@ -2,7 +2,7 @@ const { getAllPlatesAndShopsService } = require('../services/plates.service')
 
 const getAllPlatesAndShopsController = async (req, res) => {
   try {
-    const infoComplete = await getAllPlatesAndShopsService(req.body)
+    const infoComplete = await getAllPlatesAndShopsService()
     return res.status(200).json(infoComplete)
   } catch (error) {
     res.status(500).json({ message: 'Internal server error' })
